222856be1f6d20ea9e5ceaed42dc9690aed8ed1e
cmkopelowitz <cmkopelowitz@gmail.com>
2024-03-23 19:43:07 -0600

    chore: adjust schemas

--- db/schema.ts
+++ db/schema.ts
@@ -3,21 +3,32 @@ import {
   pgTable,
   serial,
   integer,
+  date,
   text,
   timestamp,
   primaryKey,
+  uuid,
 } from "drizzle-orm/pg-core";
 import type { AdapterAccount } from "@auth/core/adapters";
 
 /**
   CREATE TABLE "user" (
     id text PRIMARY KEY NOT NULL,
+    inbox_list_id uuid,
     name text,
     email text NOT NULL,
     "emailVerified" timestamp,
     image text
 );
  */
+
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  emailVerified: Date;
+  image: string;
+};
 
 export const users = pgTable("user", {
   id: text("id").notNull().primaryKey(),
@@ -107,69 +118,155 @@ export const verificationTokens = pgTable(
 );
 
 /**
- CREATE TABLE "tasks" (
-  id SERIAL PRIMARY KEY NOT NULL,
-  "userId" TEXT NOT NULL,
-  title TEXT,
-  is_completed BOOLEAN DEFAULT FALSE NOT NULL,
+ CREATE TABLE "task" (
+  id UUID PRIMARY KEY NOT NULL,
+  committed_day DATE,
   completed_at TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT NOW() NOT NULL,
   created_at TIMESTAMP DEFAULT NOW() NOT NULL,
-  FOREIGN KEY ("userId") REFERENCES "user"(id)
+  created_by TEXT NOT NULL,
+  due_date DATE,
+  important BOOLEAN,
+  last_modified_at TIMESTAMP DEFAULT NOW() NOT NULL,
+  list_id uuid,
+  note text,
+  position integer,
+  recurrence_days_of_week TEXT[],
+  recurrence_interval INTEGER,
+  recurrence_start_date DATE,
+  recurrence_type TEXT,
+  title TEXT,
+  today_position INTEGER,
+  FOREIGN KEY ("created_by") REFERENCES "user"(id) ON DELETE CASCADE,
+  FOREIGN KEY (list_id) REFERENCES "task_list"(id) ON DELETE CASCADE,
  );
  */
 
 export type Task = {
-  id: number;
-  userId: string;
-  title: string | null;
-  taskListId: number | null;
-  isCompleted: boolean;
-  isImportant: boolean;
+  id: string;
+  committedDay: Date | null;
   completedAt: Date | null;
-  updatedAt: Date;
   createdAt: Date;
+  createdBy: string;
+  important: boolean;
+  lastModifiedAt: Date;
+  listId: string | null;
+  note: string | null;
+  position: number | null;
+  recurrenceDaysOfWeek: Array<string>;
+  recurrenceInterval: number;
+  recurrenceType: string;
+  title: string | null;
+  todayPosition: number | null;
 };
 
-export const tasks = pgTable("tasks", {
-  id: serial("id").primaryKey(),
-  userId: text("userId")
+export const tasks = pgTable("task", {
+  id: uuid("id").primaryKey(),
+  committedDay: date("committed_day"),
+  completedAt: timestamp("completed_at"),
+  createdAt: date("created_at").defaultNow().notNull(),
+  createdBy: text("created_by")
     .notNull()
     .references(() => users.id, { onDelete: "cascade" }),
+  important: boolean("important").default(false).notNull(),
+  last_modified_at: timestamp("last_modified_at"),
+  listId: uuid("task_list_id").references(() => taskLists.id),
+  note: text("note"),
+  position: integer("position"),
+  recurrenceDaysOfWeek: text("recurrence_days_of_week").array(),
+  recurrenceInterval: integer("recurrence_interval"),
+  recurrenceType: text("recurrence_type"),
   title: text("title"),
-  taskListId: integer("task_list_id").references(() => taskLists.id),
-  isCompleted: boolean("is_completed").default(false).notNull(),
-  isImportant: boolean("is_important").default(false).notNull(),
-  completedAt: timestamp("completed_at"),
-  updatedAt: timestamp("updated_at").defaultNow().notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
+  todayPosition: integer("today_position"),
 });
 
 /**
- CREATE TABLE "task_lists" (
-  id SERIAL PRIMARY KEY NOT NULL,
-  "userId" TEXT NOT NULL,
-  title TEXT NOT NULL,
-  updated_at TIMESTAMP DEFAULT NOW() NOT NULL,
+ CREATE TABLE "task_list" (
+  id UUID PRIMARY KEY NOT NULL,
   created_at TIMESTAMP DEFAULT NOW() NOT NULL,
-  FOREIGN KEY ("userId") REFERENCES "user"(id)
+  created_by UUID NOT NULL,
+  last_modified_at TIMESTAMP DEFAULT NOW() NOT NULL,
+  parent_group_id UUID,
+  position INTEGER,
+  title TEXT NOT NULL,
+  FOREIGN KEY ("created_by") REFERENCES "user"(id) ON DELETE CASCADE,
+  FOREIGN KEY ("parent_group_id") REFERENCES "list_group"(id)
  );
  */
 
 export type TaskList = {
-  id: number;
-  userId: string;
-  title: string;
-  updatedAt: Date;
+  id: string;
   createdAt: Date;
+  createdBy: string;
+  lastModifiedAt: Date;
+  parentGroupId: string | null;
+  position: number | null;
+  title: string;
 };
 
-export const taskLists = pgTable("task_lists", {
-  id: serial("id").primaryKey(),
-  userId: text("userId")
+export const taskLists = pgTable("task_list", {
+  id: uuid("id").primaryKey(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  createdBy: text("created_by")
     .notNull()
     .references(() => users.id, { onDelete: "cascade" }),
+  lastModifiedAt: timestamp("last_modified_at").defaultNow().notNull(),
+  parentGroupId: uuid("parent_group_id").references(() => listGroup.id, {
+    onDelete: "cascade",
+  }),
+  position: integer("position"),
   title: text("title").notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+/**
+ CREATE TABLE "list_group" (
+  id UUID PRIMARY KEY NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+  created_by UUID NOT NULL,
+  last_modified_at TIMESTAMP DEFAULT NOW() NOT NULL,
+  position INTEGER,
+  title TEXT NOT NULL,
+  FOREIGN KEY ("created_by") REFERENCES "user"(id) ON DELETE CASCADE,
+ );
+ */
+
+export const listGroup = pgTable("list_group", {
+  id: uuid("id").primaryKey(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
+  createdBy: text("created_by")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  lastModifiedAt: timestamp("last_modified_at").defaultNow().notNull(),
+  position: integer("position"),
+  title: text("title").notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+/**
+ CREATE TABLE "step" (
+  id UUID PRIMARY KEY NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+  created_by UUID NOT NULL,
+  last_modified_at TIMESTAMP DEFAULT NOW() NOT NULL,
+  position INTEGER,
+  task_id uuid NOT NULL,
+  title TEXT NOT NULL,
+  FOREIGN KEY ("created_by") REFERENCES "user"(id) ON DELETE CASCADE,
+  FOREIGN KEY ("task_id") REFERENCES "task"(id) ON DELETE CASCADE,
+ );
+ */
+
+export const step = pgTable("step", {
+  id: uuid("id").primaryKey(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  createdBy: text("created_by")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  lastModifiedAt: timestamp("last_modified_at").defaultNow().notNull(),
+  position: integer("position"),
+  taskId: uuid("task_id")
+    .notNull()
+    .references(() => tasks.id, { onDelete: "cascade" }),
+  title: text("title").notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
